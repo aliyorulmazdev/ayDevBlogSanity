13a1e52089a956913d4afcb433c6576632a87d54
Kübra Yorulmaz <kbr.ercenk@gmail.com>
2023-10-19 17:59:03 +0300

    Settings

--- sanity.config.js
+++ sanity.config.js
@@ -3,6 +3,7 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import {codeInput} from '@sanity/code-input'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: 'fg0ja0q5',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), codeInput()],
+  plugins: [deskTool(), visionTool(), codeInput(), vercelDeployTool(),],
 
   schema: {
     types: schemaTypes,
